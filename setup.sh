4ac8fed198a3300ea9ffe267cbafa1f550b0b7c6
kyeates <kieran.yeates@4mation.com.au>
2016-07-21 17:40:14 +1000

    Add ssh config

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-readonly DOT_DIR=`pwd`
+readonly DOT_FILES_DIR=`pwd`
 
 ## ssh config
-#ln -s $DOT_DIR/config $HOME/.ssh/config
+ln -s $DOT_FILES_DIR/ssh_config $HOME/.ssh/config
 
 ## add links in bashrc. will not handle changes to that file
-if [[ -z `grep "$DOT_DIR/.bashrc" $HOME/.bashrc` ]]; then
-	echo ". $DOT_DIR/.bashrc" >> $HOME/.bashrc
+if [[ -z `grep "$DOT_FILES_DIR/.bashrc" $HOME/.bashrc` ]]; then
+	echo ". $DOT_FILES_DIR/.bashrc" >> $HOME/.bashrc
 	echo "$HOME/.bashrc linked" 
 else
 	echo "$HOME/.bashrc skipping, already updated"
 fi
+
+
